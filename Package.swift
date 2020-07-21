c16838aff363223539a80915dfbb840e41f1466c
Samuel Défago <defagos@gmail.com>
2020-07-21 16:02:46 +0200

    Update for version 6.5.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 struct PackageMetadata {
     static let version: String = "6.5.0"
-    static let checksum: String = ""
+    static let checksum: String = "748fd4bd7c6ddaa073e18e14966d0883a4bc55c35e6e0270c7c153e3d9a9a1f2"
 }
 
 let package = Package(
